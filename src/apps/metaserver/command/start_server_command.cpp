3eb5ff89f70450ea1495ee0b9aaf5a80c53922ef
SOFTBOY <softboy@fhzc.com>
2015-12-24 18:40:02 +0800

    增加服务器例子代码

--- src/apps/metaserver/command/start_server_command.cpp
+++ src/apps/metaserver/command/start_server_command.cpp
@@ -13,7 +13,13 @@
 #include "mslib/network/multi_thread_server.h"
 
 #include <QDebug>
-
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#define MAXPENDING 5 
 namespace metaserver{
 namespace command{
 
@@ -27,22 +33,53 @@ StartServerCommand::StartServerCommand(AbstractCommandRunner& runner, const Comm
    : AbstractCommand(runner, invokeMeta)
 {
 }
-
+void Die(char *mess) { perror(mess); exit(1); }
 void StartServerCommand::exec()
 {
-   qint16 port = getMetaServerListenPort();
-   if(0 == port){
-      throw ErrorInfo(QString("port %1 is not allow").arg(port));
+//   qint16 port = getMetaServerListenPort();
+//   if(0 == port){
+//      throw ErrorInfo(QString("port %1 is not allow").arg(port));
+//   }
+//   bool daemon = m_invokeMeta.getCmdArgs().value("daemon") == "true" ? true : false;
+   int serversock, clientsock;
+               struct sockaddr_in echoserver, echoclient;
+   if ((serversock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+      Die("Failed to create socket");
    }
-   bool daemon = m_invokeMeta.getCmdArgs().value("daemon") == "true" ? true : false;
-   MultiThreadServer server;
-//   QHostAddress host(QString("127.0.0.1"));
-//   server.setHost(host);
-//   server.setPort(port);
-   bool status = server.run();
-   if(!status){
-      throw ErrorInfo(server.errorString());
+   
+   /* Construct the server sockaddr_in structure */
+   memset(&echoserver, 0, sizeof(echoserver));       /* Clear struct */
+   echoserver.sin_family = AF_INET;                  /* Internet/IP */
+   echoserver.sin_addr.s_addr = htonl(INADDR_ANY);   /* Incoming addr */
+   echoserver.sin_port = htons(9796);       /* server port */
+   
+   /* Bind the server socket */
+   if (bind(serversock, (struct sockaddr *) &echoserver,
+            sizeof(echoserver)) < 0) {
+      Die("Failed to bind the server socket");
    }
+   /* Listen on the server socket */
+   if (listen(serversock, MAXPENDING) < 0) {
+      Die("Failed to listen on server socket");
+   }
+   /* Run until cancelled */
+//   while (1) {
+//      unsigned int clientlen = sizeof(echoclient);
+//      /* Wait for client connection */
+//      if ((clientsock =
+//           accept(serversock, (struct sockaddr *) &echoclient,
+//                  &clientlen)) < 0) {
+//         Die("Failed to accept client connection");
+//      }
+//   }
+   //   //   MultiThreadServer server;
+   //   ////   QHostAddress host(QString("127.0.0.1"));
+   //   ////   server.setHost(host);
+   //   ////   server.setPort(port);
+   //   //   bool status = server.run();
+   //   if(!status){
+   //      throw ErrorInfo(server.errorString());
+   //   }
 }
 
 qint16 StartServerCommand::getMetaServerListenPort()
--- src/apps/metaserver/main.cpp
+++ src/apps/metaserver/main.cpp
@@ -12,6 +12,15 @@
 #include "mslib/network/multi_thread_server.h"
 #include <QCoreApplication>
 
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+#define MAXPENDING 5 
+
 using CloudControllerApplication = metaserver::Application;
 using CommandRunner = metaserver::CommandRunner;
 using ErrorInfo = sn::corelib::ErrorInfo;
@@ -19,25 +28,52 @@ using Terminal = sn::corelib::Terminal;
 using TerminalColor = sn::corelib::TerminalColor;
 
 using metaserverlib::network::MultiThreadServer;
+
+void Die(char *mess) { perror(mess); exit(1); }
+
 int main(int argc, char *argv[])
 {
+//   int serversock, clientsock;
+//               struct sockaddr_in echoserver, echoclient;
+//   if ((serversock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+//      Die("Failed to create socket");
+//   }
+   
+//   /* Construct the server sockaddr_in structure */
+//   memset(&echoserver, 0, sizeof(echoserver));       /* Clear struct */
+//   echoserver.sin_family = AF_INET;                  /* Internet/IP */
+//   echoserver.sin_addr.s_addr = htonl(INADDR_ANY);   /* Incoming addr */
+//   echoserver.sin_port = htons(9796);       /* server port */
+   
+//   /* Bind the server socket */
+//   if (bind(serversock, (struct sockaddr *) &echoserver,
+//            sizeof(echoserver)) < 0) {
+//      Die("Failed to bind the server socket");
+//   }
+//   /* Listen on the server socket */
+//   if (listen(serversock, MAXPENDING) < 0) {
+//      Die("Failed to listen on server socket");
+//   }
+//   /* Run until cancelled */
+//   while (1) {
+//      unsigned int clientlen = sizeof(echoclient);
+//      /* Wait for client connection */
+//      if ((clientsock =
+//           accept(serversock, (struct sockaddr *) &echoclient,
+//                  &clientlen)) < 0) {
+//         Die("Failed to accept client connection");
+//      }
+//   }
    try{
-      QCoreApplication app(argc, argv);
-      
-//      app.ensureImportantDir();
-//      CommandRunner cmdrunner(app);
-//      QTimer::singleShot(0, Qt::PreciseTimer, [&cmdrunner]{
-//         cmdrunner.run();
-//      });
-//      app.createPidFile();
-
-//      app.deletePidFile();
-      MultiThreadServer server;
-   //   QHostAddress host(QString("127.0.0.1"));
-   //   server.setHost(host);
-   //   server.setPort(port);
-      bool status = server.run();
+      CloudControllerApplication app(argc, argv);
+      app.ensureImportantDir();
+      CommandRunner cmdrunner(app);
+      QTimer::singleShot(0, Qt::PreciseTimer, [&cmdrunner]{
+         cmdrunner.run();
+      });
+      app.createPidFile();
       int exitCode = app.exec();
+      app.deletePidFile();
       return exitCode;
    }catch(const ErrorInfo& errorInfo){
       QString str(errorInfo.toString());
